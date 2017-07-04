456e29eaed3e873a5b9492706ea6c7911fbea074
Nuno Mendes <nuno.laurentino.mendes@ist.utl.pt>
2017-07-04 15:38:39 -0700

    DebtCircle for visual tracker of the financial situation of an individual

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Button, Alert, Image }
+    from 'react-native';
 
 class PaymentButton extends React.Component {
   render() {
@@ -12,11 +13,28 @@ class PaymentButton extends React.Component {
   }
 }
 
+
+class DebtCircle extends React.Component {
+  render() {
+    var pic = {};
+    if(this.props.debt_status == 'owed'){
+       pic['uri'] = 'http://content.etilize.com/original/1023547217.jpg'
+    }
+    else if(this.props.debt_status == 'owing'){
+      pic['uri'] = 'http://www.iconsdb.com/icons/download/red/circle-256.gif'
+    }
+    return (
+      <Image source={pic} style={{width: 250, height: 250}}/>
+    );
+  }
+}
+
 export default class LotsOfStyles extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <PaymentButton/>
+        <DebtCircle debt_status='owed'/>
       </View>
     );
   }
